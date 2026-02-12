02575df3933d4762b24d19371c315f6073c5bbf4
eric <me@ericjuli.us>
2026-02-11 16:22:59 -0800

    Updated path name to avoid trouble

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/X4-Foundations-Station-Planner/',
+  base: '/Station-Planner-For-X4-Foundations/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
